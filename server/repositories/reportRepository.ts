26c6603ea34a6c7e94e1eb8cc4e5a7362b54bc0a
MeToRaFRev <MeToRaFRev@gmail.com>
2025-02-12 15:35:24 +0200

    removed +srv

--- server/repositories/reportRepository.ts
+++ server/repositories/reportRepository.ts
@@ -30,7 +30,7 @@ const initMongo = async () => {
     if (!mongoHost|| !mongoDb || !mongoUser || !mongoPassword) {
       throw new Error("MONGO_HOST, MONGO_DB, MONGO_USER, and MONGO_PASSWORD must be set when USE_MONGO is true."); 
     }
-    const mongoSrvUri = `mongodb+srv://${mongoUser}:${mongoPassword}@${mongoHost}/Priority?retryWrites=true&w=majority`;
+    const mongoSrvUri = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}/Priority?retryWrites=true&w=majority`;
     const client = new MongoClient();
     console.log(`Connecting to MongoDB at ${mongoSrvUri}`);
     await client.connect(mongoSrvUri);
